f6570a55e9c76e0ba9d1f0ef7d9a531cbf6622a0
okyungjin <okyungjin@gmail.com>
2022-03-25 18:04:37 +0900

    TodoInput 구현

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <form id="todo-input" @submit.prevent="submitTodoItem">
+    <label for="searchText"></label>
+    <input type="text" id="searchText" v-model="inputText">
+    <button type="submit">ADD</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: "TodoInput",
+    data() {
+      return {
+        inputText: '',
+      }
+    },
+    methods: {
+      submitTodoItem() {
+        this.$emit('submitTodoItem', this.inputText);
+      }
+    }
+  }
+</script>

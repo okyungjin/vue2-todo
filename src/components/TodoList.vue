05898b3ab62eb183b7b16862a985ad23a829bc08
okyungjin <okyungjin@gmail.com>
2022-03-25 18:04:48 +0900

    TodoList, TodoListItem 구현

---
+++ src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <TodoListItem
+            v-for="todo in todoList"
+            :title="todo"
+            :key="todo"
+            @removeTodoItem="removeTodoItem"
+    ></TodoListItem>
+  </div>
+</template>
+
+<script>
+  import TodoListItem from "./TodoListItem";
+  export default {
+    name: "TodoList",
+    components: {TodoListItem},
+    props: {
+      todoList: Array,
+    },
+    methods: {
+      removeTodoItem(targetItem) {
+        this.$emit('removeTodoItem', targetItem);
+      }
+    }
+  }
+</script>
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ title }}</p>
+    <button @click="removeTodoItem">Delete</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TodoListItem",
+    props: {
+      title: String,
+      isDone: Boolean,
+    },
+    methods: {
+      removeTodoItem() {
+        this.$emit('removeTodoItem', this.title);
+      }
+    }
+  }
+</script>
